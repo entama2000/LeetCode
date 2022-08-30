d46f8cc6dc8423c24453c8f3cb28b5b1cfc3fdcb
entama2000 <103039176+entama2000@users.noreply.github.com>
2022-08-30 15:06:29 -0400

    LeetCode1問目

---
+++ 1.two-sum.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size()-1; i++)
+        {
+            for(int j = i+1; j < nums.size(); j++)
+                if(nums[i]+nums[j] == target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    goto _EXIT;
+                }
+        }
+        _EXIT:
+        return ans;
+    }
+};
+// @lc code=end
+
