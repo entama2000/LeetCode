03a87635c64a1754d7910a988689201a3921d7e9
tendo7 <103039176+entama2000@users.noreply.github.com>
2023-08-05 14:50:40 -0400

    128 pass

--- 128.longest-consecutive-sequence.java
+++ 128.longest-consecutive-sequence.java
@@ -5,9 +5,36 @@
  */
 
 // @lc code=start
+
+import java.util.*;
+
 class Solution {
     public int longestConsecutive(int[] nums) {
-        
+        HashSet<Integer> set = new HashSet<>(nums.length);
+        for (int n : nums) set.add(Integer.valueOf(n));
+        int max = 0;
+        int i = 0;
+        while (!set.isEmpty()) {
+            if (set.contains(nums[i])) {
+                set.remove(nums[i]);
+                int count = 1;
+                int right = nums[i] + 1;
+                int left = nums[i] - 1;
+                while (set.contains(right)) {
+                    count++;
+                    set.remove(right);
+                    right++;
+                }
+                while (set.contains(left)) {
+                    count++;
+                    set.remove(left);
+                    left--;
+                }
+                max = Math.max(max, count);
+            }
+            i++;
+        }
+        return max;
     }
 }
 // @lc code=end
