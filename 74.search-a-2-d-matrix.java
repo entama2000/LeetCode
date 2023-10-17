ab6d36f926287e9860f8a6622c574f2a2605c9cc
tendo7 <103039176+entama2000@users.noreply.github.com>
2023-10-17 00:11:59 -0400

    74 pass

---
+++ 74.search-a-2-d-matrix.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=74 lang=java
+ *
+ * [74] Search a 2D Matrix
+ */
+
+// @lc code=start
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+		        return true;
+	        } else if (matrix[i][j] > target) {
+		        j--;
+	        } else {
+		        i++;
+	        }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
